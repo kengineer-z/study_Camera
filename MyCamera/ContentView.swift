7319dabe71404292cdee7a0ad92a536d1997ce72
kengineer-z <kenichiro.zaitsu@winescan.co.jp>
2022-08-15 07:22:46 +0900

    カメラ起動の実施方法を追加

--- MyCamera/ContentView.swift
+++ MyCamera/ContentView.swift
@@ -8,9 +8,46 @@
 import SwiftUI
 
 struct ContentView: View {
+    
+    @State var captureImage : UIImage? = nil
+    @State var isShowSheet = false
+    
     var body: some View {
-        Text("Hello, world!")
+        VStack {
+            Spacer()
+            if let unwrapCaptureImage = captureImage{
+                Image(uiImage: unwrapCaptureImage)
+                    .resizable()
+                    .aspectRatio(contentMode: .fit)
+                
+            }
+            Button(action: {
+                
+                //カメラ利用可能かチェック
+                if UIImagePickerController.isSourceTypeAvailable(.camera)
+                {
+                    print("カメラは利用可能")
+                    //カメラが使えるなら、isShowSheetをtrue
+                    isShowSheet = true
+                } else {
+                    print("カメラは利用不可")
+                }
+                
+            }){
+                Text("カメラを起動する")
+                    .frame(maxWidth:.infinity)
+                    .frame(height: 50)
+                    .multilineTextAlignment(.center)
+                    .background(Color.blue)
+                    .foregroundColor(Color.white)
+            }
             .padding()
+            //isPresentedで指定した状態変数がtrueのとき実行
+            .sheet(isPresented: $isShowSheet){
+                //UIImagePickerController(写真撮影）を表示
+                ImagePickerView(isShowSheet: $isShowSheet, captureImage: $captureImage)
+            }
+        }
     }
 }
 
